b26f10432b9dac150a44a155bc78519bc40b94a4
Dhud6 <93725246+Dhud6@users.noreply.github.com>
2024-02-13 16:58:36 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,397 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="author" content="Dylan Hudson (Template from DevCRUD)">
+   <title>Dylan Hudson Resume</title>
+   <link rel="icon" type="image/x-icon" href="assets/imgs/logo.svg">
+   <!-- font icons -->
+   <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+   <!-- Bootstrap + Steller main styles -->
+   <link rel="stylesheet" href="assets/css/steller.css">
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+
+   <!-- Nav Function Script -->
+   <script>
+      function myFunction(x) {
+         x.classList.toggle("change");
+      }
+   </script>
+
+   <!-- Page navigation -->
+   <nav class="navbar navbar-expand-lg navbar-light fixed-top" data-spy="affix" data-offset-top="0" >
+      <div class="container">
+         <a class="navbar-brand" href="#"><img src="assets/imgs/logo.svg" alt=""></a>
+         <button class="navbar-toggler" style="padding: 0rem;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="containerNav" style="padding: 0.25rem 0.75rem;" onclick="myFunction(this)">
+               <div class="bar1"></div>
+               <div class="bar2"></div>
+               <div class="bar3"></div>
+            </div>
+         </button>
+
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto align-items-center">
+               <li class="nav-item">
+                  <a class="nav-link" href="#home">Home</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#about">About</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#service">Service</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#contact">Contact</a>
+               </li>
+               <li class="nav-item">
+                  <a class="- btn btn-primary rounded ml-4" style="margin: 0rem !important;" href="tetris.html">Tetris</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </nav>
+   <!-- End of page navigation -->
+
+   <!-- Page Header -->
+   <header class="header" id="home">
+      <div class="container">
+         <div class="infos">
+            <h6 class="subtitle">Hello, I'm</h6>
+            <h6 class="title">Dylan Hudson</h6>
+            <p style="margin-top: .1em;">B.S. Computer Science</p>
+
+            <div class="buttons pt-3">
+               <button class="btn btn-primary rounded">HIRE ME</button>
+            </div>
+
+            <div class="socials mt-4">
+               <a class="social-item" href="javascript:void(0)"><i class="ti-facebook"></i></a>
+               <a class="social-item" href="javascript:void(0)"><i class="ti-google"></i></a>
+               <a class="social-item" href="javascript:void(0)"><i class="ti-github"></i></a>
+               <a class="social-item" href="components.php"><i class="ti-twitter"></i></a>
+            </div>
+         </div>
+         <div class="img-holder">
+            <img src="assets/imgs/Dylan.png" alt="" style="width: 75%; border-radius: .8em .8em .8em .8em;">
+         </div>
+      </div>
+
+      <!-- Header-widget -->
+      <div class="widget">
+         <div class="widget-item">
+            <h2>KeyBank</h2>
+            <p>KTOS Intern</p>
+         </div>
+         <div class="widget-item">
+            <h2>T.A.</h2>
+            <p>HPU CS Department</p>
+         </div>
+         <div class="widget-item">
+            <h2>Staples</h2>
+            <p>Tech Sales Associate</p>
+         </div>
+      </div>
+   </header>
+   <!-- End of Page Header -->
+
+   <!-- About section -->
+   <section id="about" class="section mt-3">
+      <div class="container mt-5">
+         <div class="row text-center text-md-left">
+            <div class="col-md-3">
+               <img src="assets/imgs/Dylan.png" alt="" class="img-thumbnail mb-4">
+            </div>
+            <div class="pl-md-4 col-md-9">
+               <h6 class="title">Dylan Hudson</h6>
+               <p class="subtitle">Robot Process Automation Developer</p>
+               <p>Hello, I'm Dylan Hudson, and I'm currently enrolled in the Honors Scholars Program at High Point University, where I'm pursuing 
+                  a degree in Computer Science. My passion lies in learning and programming, with proficiency in C++, Java, and Python. At High 
+                  Point University, I serve as a Teaching Assistant and tutor for three Computer Science courses.</p>
+               <p>Beyond academics, my engagement extends to the HPU eSports Club. Since my first year, I've been an integral part of the club, 
+                  contributing to the Purple (Varsity A) Team in Rainbow Six Siege and Overwatch. In January 2021, I initiated and now manage the 
+                  HPU Apex Legends team. As a Game Manager, I coordinate tryouts, form teams, plan practices, and collaborate with other Game 
+                  Managers to schedule times for in-person practice and tournaments.</p>
+               <button class="btn btn-primary rounded mt-3">HIRE ME</button>
+            </div>
+         </div>
+      </div>
+   </section>
+
+   <!-- Service section -->
+   <section id="service" class="section">
+      <div class="container text-center">
+         <h6 class="subtitle">Service</h6>
+         <h6 class="section-title mb-4">What I Do?</h6>
+         <p class="mb-5 pb-4">I am currently a High Point University student progressing towards a B.S. in Computer Science. Last summer, I 
+            interned at KeyBank as a Key Technology, Operations & Services Intern. I worked as an RPA Developer and created automations that 
+            save dozens of hours every week!</p>
+
+         <div class="row">
+            <div class="col-sm-6 col-md-3 mb-4">
+               <div class="custom-card card border">
+                  <div class="card-body">
+                     <i class="icon ti-crown"></i>
+                     <h5>Teaching Assistant</h5>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+               <div class="custom-card card border">
+                  <div class="card-body">
+                     <i class="icon ti-desktop"></i>
+                     <h5>Web Design</h5>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+               <div class="custom-card card border">
+                  <div class="card-body">
+                     <i class="icon ti-mobile"></i>
+                     <h5>App Design</h5>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+               <div class="custom-card card border">
+                  <div class="card-body">
+                     <i class="icon ti-bar-chart"></i>
+                     <h5>Programming</h5>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- End of Section -->
+
+   <!-- Skills Section -->
+   <section class="section">
+      <div class="container text-center">
+         <h6 class="subtitle">Skills</h6>
+         <h6 class="section-title mb-4">Why Choose me</h6>
+         <p class="mb-5 pb-4">I have a particular set of skills.</p>
+
+         <div class="row text-left">
+            <div class="col-sm-6">
+               <h6 class="mb-3">C++</h6>
+               <div class="progress">
+                  <div class="progress-bar bg-primary" role="progressbar" style="width: 95%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>89%</span></div>
+               </div>
+            </div>
+            <div class="col-sm-6">
+               <h6 class="mb-3">Web Design</h6>
+               <div class="progress">
+                  <div class="progress-bar bg-primary" role="progressbar" style="width: 83%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>83%</span></div>
+               </div>
+            </div>
+            <div class="col-sm-6">
+               <h6 class="mb-3">App Design</h6>
+               <div class="progress">
+                  <div class="progress-bar bg-primary" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>95%</span></div>
+               </div>
+            </div>
+            <div class="col-sm-6">
+               <h6 class="mb-3">Java</h6>
+               <div class="progress">
+                  <div class="progress-bar bg-primary" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span>90%</span></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- End of Skills sections -->
+
+   <!-- Portfolio section -->
+   <section id="portfolio" class="section">
+      <div class="container text-center">
+         <h6 class="subtitle">Portfolio</h6>
+         <h6 class="section-title mb-4">Check My Wonderful Works</h6>
+         <p class="mb-5 pb-4">Here are some of the websites I made for my Web Tech class.</p>
+
+         <div class="row">
+            <div class="col-sm-4">
+            <div class="img-wrapper">
+                  <img src="assets/imgs/firstWebpagePreview.png" alt="">
+                  <div class="overlay">
+                     <div class="overlay-infos">
+                        <h5>My First Website</h5>
+                        <a href="assets/imgs/firstWebpagePreview.png"><i class="ti-zoom-in"></i></a>
+                        <a href="https://lucy.highpoint.edu/~dhudson/csc3212/HTML/first_webpage/index.html" target="_blank"><i class="ti-link"></i></a>
+                     </div>
+                  </div>
+               </div>
+               <div class="img-wrapper">
+                  <img src="assets/imgs/SQLPreview.png" alt="">
+                  <div class="overlay">
+                     <div class="overlay-infos">
+                        <h5>Guitar Shop Website</h5>
+                        <a href="assets/imgs/SQLPreview.png"><i class="ti-zoom-in"></i></a>
+                        <a href="javascript:void(0)"><i class="ti-link"></i></a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="img-wrapper">
+                  <img src="assets/imgs/firstDesignPagePreview.png" alt="">
+                  <div class="overlay">
+                     <div class="overlay-infos">
+                        <h5>My First CSS Design</h5>
+                        <a href="assets/imgs/firstDesignPagePreview.png"><i class="ti-zoom-in"></i></a>
+                        <a href="https://lucy.highpoint.edu/~dhudson/csc3212/HTML/mypractice/design/design.html" target="_blank"><i class="ti-link"></i></a>
+                     </div>
+                  </div>
+               </div>
+               <div class="img-wrapper">
+                  <img src="assets/imgs/tipCalculatorPreview.png" alt="">
+                  <div class="overlay">
+                     <div class="overlay-infos">
+                        <h5>Tip Calculator</h5>
+                        <a href="assets/imgs/tipCalculatorPreview.png"><i class="ti-zoom-in"></i></a>
+                        <a href="javascript:void(0)"><i class="ti-link"></i></a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="img-wrapper">
+                  <img src="assets/imgs/threePagePreview.png" alt="">
+                  <div class="overlay">
+                     <div class="overlay-infos">
+                        <h5>Simple Three Page</h5>
+                        <a href="assets/imgs/threePagePreview.png"><i class="ti-zoom-in"></i></a>
+                        <a href="https://lucy.highpoint.edu/~dhudson/csc3212/HTML/simpleThreePage/index.html" target="_blank"><i class="ti-link"></i></a>
+                     </div>
+                  </div>
+               </div>
+               <div class="img-wrapper">
+                  <img src="assets/imgs/madLibPreview.png" alt="">
+                  <div class="overlay">
+                     <div class="overlay-infos">
+                        <h5>Mad Lib</h5>
+                        <a href="assets/imgs/madLibPreview.png"><i class="ti-zoom-in"></i></a>
+                        <a href="javascript:void(0)"><i class="ti-link"></i></a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+      </div>
+   </section>
+   <!-- End of portfolio section -->
+
+
+   <!-- Hire me section -->
+   <section class="bg-gray p-0 section">
+      <div class="container">
+         <div class="card bg-primary">
+            <div class="card-body text-light">
+               <div class="row align-items-center">
+                  <div class="col-sm-9 text-center text-sm-left">
+                     <h5 class="mt-3">Hire Me For Work :D</h5>
+                     <p class="mb-3">I work hard!</p>
+                  </div>
+                  <div class="col-sm-3 text-center text-sm-right">
+                     <button class="btn btn-light rounded">Hire Me!</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- End od Hire me section. -->
+
+   <!-- Contact Section -->
+   <section id="contact" class="position-relative section">
+      <div class="container text-center">
+         <h6 class="subtitle">Contact</h6>
+         <h6 class="section-title mb-4">Get In Touch With Me</h6>
+         <p class="mb-5 pb-4">Insert your information and message below and I will get back to you ASAP.</p>
+
+         <div class="contact text-left">
+            <div class="form">
+               <h6 class="subtitle">Available 24/7</h6>
+               <h6 class="section-title mb-4">Get In Touch</h6>
+               <form action="javascript:void(0)" method="post">
+                  <div class="form-group">
+                     <input type="email" class="form-control" name="email_input_1" id="email_input_1" aria-describedby="emailHelp" placeholder="Enter email" required>
+                  </div>
+                  <div class="form-group">
+                     <input type="email" class="form-control" name="email_input_2" id="email_input_2" placeholder="Enter email again" required>
+                  </div>
+                  <div class="form-group">
+                     <textarea name="message_details" id="message_details" cols="30" rows="5" class="form-control" placeholder="Message"></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-block rounded w-lg">Send Message</button>
+               </form>
+            </div>
+            <div class="contact-infos">
+               <div class="item">
+                  <i class="ti-location-pin"></i>
+                  <div class="">
+                     <h5>Location</h5>
+                     <p> Ohio</p>
+                  </div>
+               </div>
+               <div class="item">
+                  <i class="ti-mobile"></i>
+                  <div>
+                     <h5>Phone Number</h5>
+                     <p>Contact Me For Info</p>
+                  </div>
+               </div>
+               <div class="item">
+                  <i class="ti-email"></i>
+                  <div class="mb-0">
+                     <h5>Email Address</h5>
+                     <p>dhudson@highpoint.edu</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div id="map">
+         <iframe src="https://snazzymaps.com/embed/487209"></iframe>
+      </div>
+   </section>
+   <!-- End of Contact Section -->
+
+   <!-- Page Footer -->
+   <footer class="page-footer">
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-sm-6">
+               <p>Copyright &copy; <?php echo date('Y') ?> Dylan Hudson</p>
+            </div>
+            <div class="col-sm-6">
+               <div class="socials">
+                  <a class="social-item" href="javascript:void(0)"><i class="ti-facebook"></i></a>
+                  <a class="social-item" href="javascript:void(0)"><i class="ti-google"></i></a>
+                  <a class="social-item" href="javascript:void(0)"><i class="ti-github"></i></a>
+                  <a class="social-item" href="javascript:void(0)"><i class="ti-twitter"></i></a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </footer>
+   <!-- End of page footer -->
+
+   <!-- core  -->
+   <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+   <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+   <!-- bootstrap 3 affix -->
+   <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+   <!-- steller js -->
+   <script src="assets/js/steller.js"></script>
+
+</body>
+
+</html>
